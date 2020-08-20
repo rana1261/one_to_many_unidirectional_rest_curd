ecc98ea2b4db290e31059362490e61221fed121e
sohel Rana <ranacse2018@gmail.com>
2020-08-20 15:40:22 +0600

    Basic configuration

---
+++ src/main/java/com/demo/one_to_many/OneToManyUnidirectionalRestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.one_to_many;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyUnidirectionalRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToManyUnidirectionalRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/one_to_many/OneToManyUnidirectionalRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.one_to_many;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyUnidirectionalRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
